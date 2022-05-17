8ef1010993270fd450b3c8588eee36a9c7733d9a
robert <nhnam13@cmcglobal.vn>
2022-05-17 17:11:17 +0700

    config swagger

--- src/main/java/fpt/edu/capstone/exception/ResponseEntityBuilder.java
+++
@@ -1,9 +0,0 @@
-package fpt.edu.capstone.exception;
-
-import org.springframework.http.ResponseEntity;
-
-public class ResponseEntityBuilder {
-    public static ResponseEntity<Object> build(ErrorResponse apiError) {
-        return new ResponseEntity<>(apiError, apiError.getStatus());
-    }
-}
