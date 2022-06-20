dc190fcd5cb989d5f0cc6f8ba9d5f19c2585721e
NamNH <hoainam.fptu@gmail.com>
2022-06-20 10:24:19 +0700

    api define for be

--- src/main/java/fpt/edu/capstone/controller/CompanyController.java
+++ src/main/java/fpt/edu/capstone/controller/CompanyController.java
@@ -3,14 +3,25 @@ package fpt.edu.capstone.controller;
 import fpt.edu.capstone.utils.ResponseData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/company")
 public class CompanyController {
 
-    @GetMapping("get-list-company")
+    @GetMapping("/get-list-company")
     public ResponseData getListCompany(){
+        return null;
+    }
+
+    @GetMapping("/find-company")
+    public ResponseData findCompany(@RequestParam String companyName){
+        return null;
+    }
+
+    @GetMapping("/create-company")
+    public ResponseData createCompany(){
         return null;
     }
 }
--- src/main/java/fpt/edu/capstone/controller/RecruiterController.java
+++ src/main/java/fpt/edu/capstone/controller/RecruiterController.java
@@ -29,4 +29,60 @@ public class RecruiterController {
             return new ResponseData(Enums.ResponseStatus.ERROR.getStatus(), ResponseMessageConstants.ERROR);
         }
     }
+
+    @PutMapping("/account-authen-level")
+    public ResponseData changeLevelAuthenAccount(){
+        //cấp độ xác thực account của recruiter ( update trong db khi đã xác thực xong 1 cấp độ)
+        return null;
+    }
+
+    @PutMapping("/update-recruiter-profile")
+    public ResponseData updateProfile(){
+        //cập nhật thông tin profile của rec
+        return null;
+    }
+
+    @PostMapping("/prove-recruiter")
+    public ResponseData insertProveOfRecruiter(){
+        //thêm giấy phép kinh doanh or thẻ nhân viên... chứng minh là nhà tuyển dụng
+        return null;
+    }
+
+    //TODO : Dashboard
+    @PostMapping("/create-campaign")
+    public ResponseData createCampaign(){
+        return null;
+    }
+
+    @PutMapping("/update-campaign")
+    public ResponseData updateCampaign(){
+        return null;
+    }
+
+    @DeleteMapping("/delete-campaign")
+    public ResponseData deleteCampaign(){
+        return null;
+    }
+
+    @GetMapping("/get-total-campaign-open")
+    public ResponseData totalCampaign(){
+        //lấy ra total count các chiến dịch
+        return null;
+    }
+
+    @GetMapping("/get-detail-campaign")
+    public ResponseData detailCampaign(){
+        //lấy ra chi tiết chiến dịch
+        return null;
+    }
+
+    @GetMapping("/get-total-cv-applied")
+    public ResponseData totalCVApplied(){
+        return null;
+    }
+
+    @GetMapping("/get-detail-cv")
+    public ResponseData detailCv(){
+        return null;
+    }
 }
