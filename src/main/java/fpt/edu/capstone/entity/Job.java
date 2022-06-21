fc78b540f5a02576b2668c9f15c3bf9fd805842e
Tieu Mai <lethitieumai@gmail.com>
2022-06-21 22:25:10 +0700

    [HC-33] add job entity

---
+++ src/main/java/fpt/edu/capstone/entity/Job.java
@@ -0,0 +1,88 @@
+package fpt.edu.capstone.entity;
+
+import fpt.edu.capstone.utils.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "job")
+public class Job extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "company_id")
+    private String companyId;
+
+    @Column(name = "job_name")
+    private String jobName;
+
+    @Column(name = "work_place")
+    private String workPlace;
+
+    @Column(name = "work_form")
+    private String workForm;
+
+    @Column(name = "start_date")
+    private Timestamp startDate;
+
+    @Column(name = "end_date")
+    private Timestamp endDate;
+
+    @Column(name = "from_salary")
+    private long fromSalary;
+
+    @Column(name = "to_salary")
+    private long toSalary;
+
+    @Column(name = "number_recruits")
+    private String numberRecruits;
+
+    @Column(name = "rank")
+    private String rank;
+
+    @Column(name = "experience")
+    private String experience;
+
+    @Column(name = "gender")
+    private boolean gender;
+
+    @Column(name = "job_description")
+    private String jobDescription;
+
+    @Column(name = "job_requirement")
+    private String jobRequirement;
+
+    @Column(name = "benifit")
+    private String benifit;
+
+    @Column(name = "career_id")
+    private String careerId;
+
+    @Column(name = "hashtag_id")
+    private String hashtagId;
+
+    @Column(name = "is_deleted")
+    private int isDeleted;
+
+    @Column(name = "is_popular_job")
+    private boolean isPopularJob;
+
+    @Column(name = "is_new_job")
+    private boolean isNewJob;
+
+    @Column(name = "is_urgent_job")
+    private boolean isUrgentJob;
+
+    @Column(name = "recruiter_id")
+    private long recruiterId;
+}
